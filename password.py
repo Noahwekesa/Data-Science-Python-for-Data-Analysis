3a7e7cbc24658ebf9acc83658136dc0136824fe7
noah wekesa <57433233+Noahwekesa@users.noreply.github.com>
2023-03-06 18:50:57 +0300

    Delete password.py

--- password.py
+++
@@ -1,8 +0,0 @@
-#program for checking password
-for x in range(4):
-    password = input("Enter password: ")
-    if password == "1234":
-        break
-    print("error, this is not correct ... try again!")
-else:
-        print("the process is completed. Thanks!")
